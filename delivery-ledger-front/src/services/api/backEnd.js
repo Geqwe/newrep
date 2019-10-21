86cea8c1c5ed2c4b122a4ee1101896ab51f2f86d
Giorgos Kletsas <georgeklgeo@gmail.com>
2019-10-21 11:34:23 +0300

    Upload New File

---
+++ delivery-ledger-front/src/services/api/backEnd.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export default {
+    postCalls(route, data) {
+      console.log(data);
+      return axios.post('http://localhost:5000' + route, data)
+        .then(response => {
+          return response;
+        })
+    },
+    getCalls(route) {
+      return axios.get('https://localhost:5000' + route)
+        .then(response => {
+          return response;
+        })
+    },
+    deleteCalls(route) {
+      return axios.delete('http://localhost:5000' + route)
+        .then(response => {
+          return response;
+        })
+    },
+    putCalls(route, data) {
+      return axios.put('http://localhost:5000' + route, data)
+        .then(response => {
+          return response;
+        })
+    }
+  }
