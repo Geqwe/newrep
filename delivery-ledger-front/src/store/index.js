4b7e7795307801a127605eaa2192d94255e4929d
Giorgos Kletsas <georgeklgeo@gmail.com>
2019-12-16 13:20:22 +0200

    Update index.js

--- delivery-ledger-front/src/store/index.js
+++ delivery-ledger-front/src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import account from './modules/account';
+import createDeliveryNote from './modules/createDeliveryNote';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    account
+    account,
+    createDeliveryNote
   }
 });
