179679cab8aa6ef6e350ed98d521abf74c112a2b
Giorgos Kletsas <georgeklgeo@gmail.com>
2019-12-16 13:17:14 +0200

    Update App.vue

--- delivery-ledger-front/src/App.vue
+++ delivery-ledger-front/src/App.vue
@@ -5,8 +5,10 @@
         <v-toolbar-title style="width: 180px" class="ml-5">Delivery Service</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-btn flat small dark @click="gotoHome()">Home</v-btn>
-        <v-btn flat small dark @click="gotoLog()">Login</v-btn>
-        <v-btn flat small dark @click="gotoReg()">Register</v-btn>
+        <v-btn v-if="loggedIn == false" flat small dark @click="gotoLog()">Login</v-btn>
+        <v-btn v-if="loggedIn == false" flat small dark @click="gotoReg()">Register</v-btn>
+        <v-btn v-show="loggedIn" flat small dark @click="gotoNote()">ΔΕΛΤΙΟ</v-btn>
+        <v-btn v-show="loggedIn" flat small dark @click="gotoNotes()">ΚΑΤΑΧΩΡΗΣΕΙΣ</v-btn>
         <v-btn v-show="loggedIn" flat small dark @click="logout()">Logout</v-btn>
       </v-toolbar>
       <router-link v-if="loggedIn" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
@@ -34,7 +36,13 @@ export default {
       this.$router.replace({ name: "register"});
     },
     gotoHome() {
+      this.$router.replace({ name: "home"});
+    },
+    gotoNote() {
       this.$router.replace({ name: "homepage"});
+    },
+    gotoNotes() {
+      this.$router.replace({ name: "deliverynotes"});
     }
   },
   computed: {
